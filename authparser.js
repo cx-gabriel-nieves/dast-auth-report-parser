cc37ca05982270ad1267989544ec3646f9d037b4
Gabriel Nieves <gabriel.nieves@checkmarx.com>
2026-09-15 10:33:17 -0400

    Check for error during login steps

--- authparser.js
+++ authparser.js
@@ -1381,6 +1381,13 @@ function displaySummary(run_key) {
     });
     $("#summary .content").append(poll_summary);
 
+    $.each($("[id^='login_steps_']"), function(key, el) {
+        console.log("_check_error", el, key);
+        if ($(el).find("h4:first").text().match(/Error/) && $(".incomplete-run-error-warning").length == 0) {
+            $("#summary .content").prepend(`<a href="#${$(el).attr("id")}" class="message-badge bad incomplete-run-error-warning">Critical Error: The authentication steps did not complete. The final step ends in an error</a>`);
+        }
+    });
+
     analyzeConclusion();
 }
 
